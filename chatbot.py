556a1f2da6ea93682457a92ac8cd608e6969fee4
pppseason <season.peng@gmail.com>
2017-07-31 22:12:17 +0800

    wechatbot from tuling by python3

---
+++ chatbot.py
@@ -0,0 +1,28 @@
+import itchat
+import  requests
+
+TULING_KEY = '33d276e93843c8385e94a08c6c7ec878'
+
+def get_reply(msg):
+    apiUrl = 'http://www.tuling123.com/openapi/api'
+    data = {
+        'key' : TULING_KEY,
+        'info' : msg,
+#        'loc' : '广东省惠州市',
+        'userid' : '大神彭',
+    }
+    try:
+        r = requests.post(apiUrl,data=data).json()
+        return r.get("text")
+    except:
+        return None
+
+@itchat.msg_register(itchat.content.TEXT)
+def tuling_reply(msg):
+    defaultReply = '呵'
+    reply = get_reply(msg['Text'])
+    return reply or defaultReply
+
+
+itchat.auto_login(hotReload=False,enableCmdQR=2)
+itchat.run()
